0e2fe058c57c49db657a8659578e5d40154d2c9f
JDMCM <jmcmahon@trinity.edu>
2025-03-21 20:24:38 -0500

    Work in progress

--- server.js
+++ server.js
@@ -3,21 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var homeRouter = require('./routes/home');
-var indexRouter = require('./routes/index');
-var recipeRouter = require('./routes/recipe');
-var formRouter = require('./routes/form');
-
 var app = express();
+
+const db = require('./db');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.use(homeRouter);
-app.use(indexRouter);
-app.use(recipeRouter);
-app.use(formRouter);
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get("/index",(req,res) => {
+    db.query("SELECT name FROM recipes", (err,result) => {
+        if(err) throw err;
+        res.render("index"),{recipes: result}
+    })
+});
+
+
 
 app.listen(4000)
